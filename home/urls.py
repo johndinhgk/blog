850045ad4c376ff514a5daae6a5ff4e95e81f5d0
Nguyen Quoc Dinh <johndinhgk@gmail.com>
2021-02-14 19:45:33 +0900

    Update Blog

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',views.index),
     path('home/contact/',views.contact,name='contact'),
-    path('home/register/',views.register,name='register')
+    path('home/register/',views.register,name='register'),
+    path('home/login/',auth_views.LoginView.as_view(template_name="pages/login.html"),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(next_page="/"),name='logout')
 ]
 
